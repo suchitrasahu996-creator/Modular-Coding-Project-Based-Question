fb34e91a187888361fb7fc88a2e9f596c1aee63b
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2025-12-11 17:44:16 +0530

    adding the codes

---
+++ script/footer.js
@@ -0,0 +1,7 @@
+export function footer(){
+    return ` 
+    <footer style="background:pink; color:white; padding:10px; text-algin:center;">
+        @ 2025 Modular Project
+    </footer>
+    `;
+}
---
+++ script/navbar.js
@@ -0,0 +1,10 @@
+export function navbar(){
+    return`
+    <nav style="background= white; color:white; padding:10px;">
+        <a href="index.html" style="color:white; margin-right:10px">Home</a>
+        <a href="signup.html" style="color:white  margin-right:10px">Signup</a>
+        <a href="login.html" style="color:white margin-right:10px">Login</a>
+        <a href="todos.html" style="color:white">Todos</a>
+    </nav>
+    `;
+}
